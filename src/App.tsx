c427c62df780f3d431135bc818686b1120e2c2b1
Ryslan Ykhlin <ryslanukhlin@gmail.com>
2022-04-16 18:28:54 +0300

    start test task

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<h1>home page</h1>} />
+                <Route path="/aboutMe" element={<h1>AboutMe</h1>} />
+                <Route path="/photos/:id" element={<h1>photo page</h1>} />
+            </Routes>
+        </>
+    );
+};
 
 export default App;
